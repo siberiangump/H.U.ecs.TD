d79dded719c80a9b6e308d95ee3c26157faf1a96
sokolov.anton <>
2018-12-22 11:18:37 +0200

    [*] dermo

---
+++ Assets/MonitorPointer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonitorPointer : MonoBehaviour
+{
+    public Transform Target;
+    public Camera Camera;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit raycastHit;
+        bool rayHitSomeThing = Physics.Raycast(ray, out raycastHit);
+        Debug.Log(raycastHit.point);
+        if (rayHitSomeThing)
+            Target.position = raycastHit.point;
+    }
+}
---
+++ Assets/Scripts/MouseController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+
+public class MouseController : MonoBehaviour
+{
+    public Camera Camera;
+    public NavMeshAgent TargetAgent;
+    public Button Area;
+
+    private void Awake()
+    {
+        Area.onClick.AddListener(TryMove);
+    }
+
+    // Update is called once per frame
+    public void TryMove()
+    {
+        if (!TargetAgent)
+            return;
+        Ray ray = Camera.ScreenPointToRay(Input.mousePosition);
+        Debug.Log(Input.mousePosition);
+        RaycastHit raycastHit;
+        bool rayHitSomeThing = Physics.Raycast(ray, out raycastHit);
+        Debug.Log(raycastHit.point);
+        if (rayHitSomeThing)
+            TargetAgent.SetDestination(raycastHit.point);
+    }
+
+    public void SetAgent(NavMeshAgent agent)
+    {
+        TargetAgent = agent;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            TryMove();
+        }
+    }
+}
