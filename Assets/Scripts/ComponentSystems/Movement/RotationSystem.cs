3f0bf1ce4beb1d26a6d5d6854eecc9e98af714c2
andrej.senchov <andrej.senchov@murka.com>
2019-03-09 14:20:35 +0200

    [+] for rotation testing

---
+++ Assets/Scripts/ComponentSystems/Movement/RotationSystem.cs
@@ -0,0 +1,32 @@
+﻿using Unity.Entities;
+using Unity.Collections;
+using Unity.Transforms;
+using Unity.Mathematics;
+using Assets.Scripts.MainLogic.Weapons;
+
+public class RotationSystem : ComponentSystem
+{
+    private struct RotationComponents
+    {
+        public readonly int Length;
+        public ComponentDataArray<Rotation> Rotation;       
+    }
+
+  //  [Inject] RotationComponents RotationInject;
+
+    protected override void OnUpdate()
+    {
+        //for (int i = 0; i < RotationInject.Length; i++)
+        //{
+        //    Rotation rot = CreaateRotationComponent();
+        //    RotationInject.Rotation[i] = rot;
+        //}
+    }
+
+    private Rotation CreaateRotationComponent()
+    {
+        Rotation rot = new Rotation();
+        rot.Value = quaternion.Euler(WeaponsConfig.PistolBulletRotation);
+        return rot;
+    }
+}
---
+++ Assets/Scripts/MainLogic/Weapons/RotationSetter.cs
@@ -0,0 +1,16 @@
+﻿using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Assets.Scripts.MainLogic.Weapons
+{
+    class RotationSetter : MonoBehaviour
+    {
+        [SerializeField] private float3 Rotation;
+
+        public void Update()
+        {
+            WeaponsConfig.PistolBulletRotation = Rotation;
+        }
+    }
+}
